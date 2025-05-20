d582f51d802625ff7ef02e9198c81c1b8bef88af
Kidus <kidus.z3rd@gmail.com>
2025-05-20 18:24:40 +0300

    ui updated

--- src/app/mycomponents/hero.tsx
+++ src/app/mycomponents/hero.tsx
@@ -1,5 +1,4 @@
 "use client";
-import React from "react";
 import { Linkedin, Github, Mail, Send } from "lucide-react";
 import { Marcellus } from "next/font/google";
 import { motion } from "framer-motion";
@@ -20,7 +19,7 @@ export default function HeroSection() {
               <img
                 className="w-8 h-8 xmd:w-16 xmd:h-16 bottom-0 "
                 src="/common_files/images/waving.png"
-                alt="fayida"
+                alt="kidus"
               />
             </div>
             <div className="py-7">
@@ -30,25 +29,33 @@ export default function HeroSection() {
             </div>
             <div className="flex space-x-4">
               <a
-                href=""
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/in/kidus-tesfaye-b86766b4/"
                 className=" hover:text-primaryColor hover:border-primaryColor border-2 p-2 border-gray-800 rounded-xl"
               >
                 <Linkedin />
               </a>
               <a
-                href=""
+                href="https://github.com/KidusThethird"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="hover:text-primaryColor hover:border-primaryColor border-2 p-2 border-gray-800 rounded-xl"
               >
                 <Github />
               </a>
               <a
-                href=""
+                href="https://t.me/kidustesfaye3"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="hover:text-primaryColor hover:border-primaryColor border-2 p-2 border-gray-800 rounded-xl"
               >
                 <Send />
               </a>
               <a
-                href=""
+                href="mailto:kidus.z3rd@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="hover:text-primaryColor hover:border-primaryColor border-2 p-2 border-gray-800 rounded-xl"
               >
                 <Mail />
@@ -56,32 +63,25 @@ export default function HeroSection() {
             </div>
           </div>
         </div>
-        {/* <div className=" flex justify-center items-center p-10">
-          <img
-            className="w-96 rounded-full border-4 border-gray-800"
-            src="/common_files/images/mypicture.jpg"
-            alt="fayida"
-          />
-        </div> */}
 
         <div className="flex justify-center items-center p-10 relative">
           <motion.img
             className="w-52 xmd:w-96 rounded-full border-4 border-gray-800"
             src="/common_files/images/mypicture.jpg"
-            alt="fayida"
+            alt="kidus"
             animate={{
               scale: [1, 1.05, 1], // Pulsing in size
               rotate: [0, 2, -2, 0], // Slow rotation
             }}
             transition={{
               duration: 12, // Slow and smooth animation
-              repeat: Infinity,
+              repeat: Number.POSITIVE_INFINITY,
               ease: "easeInOut",
             }}
           />
 
           {/* Fixed Text on Image */}
-          <div className="hidden xmd:block absolute text-white text-center font-semibold rig text-sm bg-opacity-70 bg-primaryColor  p-2 rounded-md bottom-16 right-10 transform -translate-x-1/2">
+          <div className="hidden xmd:block absolute text-white text-center font-semibold text-sm bg-opacity-70 bg-primaryColor p-2 rounded-md bottom-16 right-10 transform -translate-x-1/2">
             Computer Engineering <br /> Graduate
           </div>
         </div>
